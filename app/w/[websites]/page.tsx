c0bf7634ccb2875f1153b4d9096539d10a2ffca9
Arup53 <arupkumarhowlader12@gmail.com>
2025-06-03 21:11:40 +0600

    fetch single domain all analytics in dynamic route

--- app/w/[websites]/page.tsx
+++ app/w/[websites]/page.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams, useSearchParams } from "next/navigation";
 
 const Page = () => {
   // --- have to check if a user is present fron next auth hook
 
   const { websites } = useParams();
+  const [loading, setLoading] = useState(false);
+  const [analyticsData, setAnalyticsData] = useState([]);
   console.log(websites);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    fetchAnalytics();
+  }, [websites]);
+
+  const fetchAnalytics = async () => {
+    const res = await fetch(
+      `/api/getSingleWebsiteAnalytics?website_name=${websites}`
+    );
+    const data = await res.json();
+    console.log(data);
+  };
 
   return <div></div>;
 };
